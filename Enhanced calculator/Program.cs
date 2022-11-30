cc18ade5234aa8649df2e1f02c9ca6943e648dc0
eglezyge <eglezuka@gmail.com>
2022-11-30 20:22:09 +0200

    With exception handling

--- Enhanced calculator/Program.cs
+++ Enhanced calculator/Program.cs
@@ -6,38 +6,37 @@ class Enhanced
     {
         double num1;
         double num2;
-        for (; ; )
+        while (true)
         {
             try
             {
-                Console.WriteLine("Enter 1st number:");
+                Console.WriteLine("Enter 1st number: ");
                 num1 = Convert.ToDouble(Console.ReadLine());
                 break;
             }
-            catch
+            catch 
             {
-                Console.WriteLine("It has to be a number, try again.");
+                Console.WriteLine("It has to be a numbner, try again.");
             }
         }
-        for (; ; )
+        while (true)
         {
             try
             {
-                Console.WriteLine("Enter 2nd number:");
+                Console.WriteLine("Enter 2nd number: ");
                 num2 = Convert.ToDouble(Console.ReadLine());
                 break;
             }
             catch
             {
-                Console.WriteLine("It has to be a number, try again.");
+                Console.WriteLine("It has to be a number. Try again.");
             }
         }
         string? char1;
         bool badChar = true;
-        bool zeroDivision = false;
-        for (; badChar; )
+        while (badChar)
         {
-            Console.WriteLine("Enter one of five characters for wanted operation: * / % + or - :");
+            Console.WriteLine("Enter one of five characters: * / % + or - :");
             char1 = Console.ReadLine();
             badChar = false;
             if (char1 == "*")
@@ -50,82 +49,165 @@ class Enhanced
             {
                 if (num2 == 0)
                 {
-                    zeroDivision = true;
+                    Console.WriteLine("Division by 0 is not possible. Chose another character.");
+                    badChar = true;
                 }
                 else
                 {
                     double resultDiv = num1 / num2;
-                    Console.WriteLine("You chose division. The result is: " + resultDiv);
+                    Console.WriteLine("You chose division. The resultis: " + resultDiv);
                     break;
                 }
             }
             else if (char1 == "%")
             {
-                double resultMod = num1 % num2;
-                Console.WriteLine("You chose modulo. The result is: " + resultMod);
+                double resultModulo = num1 % num2;
+                Console.WriteLine("You chose modulo. The result is: " + resultModulo); 
                 break;
             }
             else if (char1 == "+")
             {
                 double resultAdd = num1 + num2;
-                Console.WriteLine("You chose addition. The result is: " + resultAdd);
+                Console.WriteLine("You chose add. The result: " + resultAdd);
                 break;
             }
             else if (char1 == "-")
             {
-                double resultSubtract = num1 - num2;
-                Console.WriteLine("You chose subtraction. The result is: " + resultSubtract);
+                double resultSub = num1 - num2;
+                Console.WriteLine("You chose sub. The result: " + resultSub);
                 break;
-            }
-            badChar = true;
-            if (zeroDivision == true)
-            {
-                Console.WriteLine("Division by zero is not possible.");
             }
             else
             {
-                Console.WriteLine("It has to be the right character, try again.");
+                Console.WriteLine("It has to be the right character.");
+                badChar = true;
             }
+            
         }
+       
     }
 }
 
 
-  
-            //Console.WriteLine("Please enter a number");
-            //var num1str = Console.ReadLine();
-            //Console.WriteLine("Please enter another number");
-            //var num2str = Console.ReadLine();
-            //Console.WriteLine("Enter one of five characters for wanted operation: * / % + or - ");
-            //var char1 = Console.ReadLine();
-            //double num1 = Double.Parse(num1str);
-            //double num2 = Double.Parse(num2str);
-            //if (char1 == "*")
-            //{ 
-            //    double resultMult = num1 * num2;
-            //    Console.WriteLine("You chose multiplication. The result is:" + resultMult);
-            //}
-            //else if (char1 == "/")
-            //{
-            //    double resultDiv = num1 / num2;
-            //    Console.WriteLine("You chose division. The result is:" + resultDiv);
-            //}
-            //else if (char1 == "%")
-            //{
-            //    double resultMod = num1 % num2;
-            //    Console.WriteLine("You chose modulo. The result is:" + resultMod);
-            //}
-            //else if (char1 == "+")
-            //{
-            //    double resultAdd = num1 + num2;
-            //    Console.WriteLine("You chose addition. The result is:" + resultAdd);
-            //}
-            //else if (char1 == "-")
-            //{
-            //    double resultSubtract = num1 - num2;
-            //    Console.WriteLine("You chose subtraction. The result is:" + resultSubtract);
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Operation is not valid, enter the right character.");
-            //}
+//double num1;
+//double num2;
+//for (; ; )
+//{
+//    try
+//    {
+//        Console.WriteLine("Enter 1st number:");
+//        num1 = Convert.ToDouble(Console.ReadLine());
+//        break;
+//    }
+//    catch
+//    {
+//        Console.WriteLine("It has to be a number, try again.");
+//    }
+//}
+//for (; ; )
+//{
+//    try
+//    {
+//        Console.WriteLine("Enter 2nd number:");
+//        num2 = Convert.ToDouble(Console.ReadLine());
+//        break;
+//    }
+//    catch
+//    {
+//        Console.WriteLine("It has to be a number, try again.");
+//    }
+//}
+//string? char1;
+//bool badChar = true;
+//bool zeroDivision = false;
+//for (; badChar == true; )
+//{
+//    Console.WriteLine("Enter one of five characters for wanted operation: * / % + or - :");
+//    char1 = Console.ReadLine();
+//    badChar = false;
+//    if (char1 == "*")
+//    {
+//        double resultMult = num1 * num2;
+//        Console.WriteLine("You chose multiplication. The result is: " + resultMult);
+//        break;
+//    }
+//    else if (char1 == "/")
+//    {
+//        if (num2 == 0)
+//        {
+//            zeroDivision = true;
+//        }
+//        else
+//        {
+//            double resultDiv = num1 / num2;
+//            Console.WriteLine("You chose division. The result is: " + resultDiv);
+//            break;
+//        }
+//    }
+//    else if (char1 == "%")
+//    {
+//        double resultMod = num1 % num2;
+//        Console.WriteLine("You chose modulo. The result is: " + resultMod);
+//        break;
+//    }
+//    else if (char1 == "+")
+//    {
+//        double resultAdd = num1 + num2;
+//        Console.WriteLine("You chose addition. The result is: " + resultAdd);
+//        break;
+//    }
+//    else if (char1 == "-")
+//    {
+//        double resultSubtract = num1 - num2;
+//        Console.WriteLine("You chose subtraction. The result is: " + resultSubtract);
+//        break;
+//    }
+//    badChar = true;
+//    if (zeroDivision == true)
+//    {
+//        Console.WriteLine("Division by zero is not possible.");
+//    }
+//    else
+//    {
+//        Console.WriteLine("It has to be the right character, try again.");
+//    }
+//}
+
+
+//Console.WriteLine("Please enter a number");
+//var num1str = Console.ReadLine();
+//Console.WriteLine("Please enter another number");
+//var num2str = Console.ReadLine();
+//Console.WriteLine("Enter one of five characters for wanted operation: * / % + or - ");
+//var char1 = Console.ReadLine();
+//double num1 = Double.Parse(num1str);
+//double num2 = Double.Parse(num2str);
+//if (char1 == "*")
+//{ 
+//    double resultMult = num1 * num2;
+//    Console.WriteLine("You chose multiplication. The result is:" + resultMult);
+//}
+//else if (char1 == "/")
+//{
+//    double resultDiv = num1 / num2;
+//    Console.WriteLine("You chose division. The result is:" + resultDiv);
+//}
+//else if (char1 == "%")
+//{
+//    double resultMod = num1 % num2;
+//    Console.WriteLine("You chose modulo. The result is:" + resultMod);
+//}
+//else if (char1 == "+")
+//{
+//    double resultAdd = num1 + num2;
+//    Console.WriteLine("You chose addition. The result is:" + resultAdd);
+//}
+//else if (char1 == "-")
+//{
+//    double resultSubtract = num1 - num2;
+//    Console.WriteLine("You chose subtraction. The result is:" + resultSubtract);
+//}
+//else
+//{
+//    Console.WriteLine("Operation is not valid, enter the right character.");
+//}
